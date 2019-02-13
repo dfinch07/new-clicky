4c4ec327692cae146a758a4968eb81ad96228233
DVictory <derrickfinch35@gmail.com>
2019-02-12 20:50:32 -0500

    Updates

---
+++ precache-manifest.618b815bb11041f98491e0cb1f8674cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b16ade6246e5b83401c0",
+    "url": "/new-clicky/static/js/runtime~main.b16ade62.js"
+  },
+  {
+    "revision": "7a9e6c96f349a5ed2fb7",
+    "url": "/new-clicky/static/js/main.7a9e6c96.chunk.js"
+  },
+  {
+    "revision": "68f68bb0bda164650735",
+    "url": "/new-clicky/static/js/2.68f68bb0.chunk.js"
+  },
+  {
+    "revision": "7a9e6c96f349a5ed2fb7",
+    "url": "/new-clicky/static/css/main.e4af2d4f.chunk.css"
+  },
+  {
+    "revision": "2d6c6601392682b7520032c01f37e079",
+    "url": "/new-clicky/index.html"
+  }
+];
